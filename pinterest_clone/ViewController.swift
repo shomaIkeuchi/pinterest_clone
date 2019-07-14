d29a70a61c57960de41c3adbbc8b8e511a6d7f37
shoma <shoma.aptx4869@gmail.com>
2019-07-15 03:04:13 +0900

    Initial Commit

---
+++ pinterest_clone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pinterest_clone
+//
+//  Created by 池内将真 on 2019/07/15.
+//  Copyright © 2019 WAKUike. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ pinterest_cloneTests/pinterest_cloneTests.swift
@@ -0,0 +1,34 @@
+//
+//  pinterest_cloneTests.swift
+//  pinterest_cloneTests
+//
+//  Created by 池内将真 on 2019/07/15.
+//  Copyright © 2019 WAKUike. All rights reserved.
+//
+
+import XCTest
+@testable import pinterest_clone
+
+class pinterest_cloneTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
